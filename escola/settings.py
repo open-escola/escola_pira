3779f13ff781e2ef22e4a05bf0463d6794539778
Michel Metran <michelmetran@gmail.com>
2021-08-31 22:55:09 -0300

    Django Project

--- escola/settings.py
+++ escola/settings.py
@@ -134,3 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.CustomUser'
 AUTHENTICATION_BACKENDS = ['core.email_backend.EmailBackend']
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
