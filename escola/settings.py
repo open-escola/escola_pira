284e0e98b75128f01b4395f58702438a515d8a51
Michel Metran <michelmetran@gmail.com>
2021-11-24 01:15:52 -0300

    Ajustes

--- escola/settings.py
+++ escola/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
