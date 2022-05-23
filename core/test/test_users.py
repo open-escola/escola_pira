ad2710241cd06204244ec9811009628e8ac01850
JvPelai <jv.pelai@gmail.com>
2022-05-23 15:19:55 -0300

    exemplo unit test

---
+++ core/test/test_users.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from core.models import CustomUser
+# Create your tests here.
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        CustomUser.objects.create_user(
+            username='sseber',
+            password='12345',
+            email='sseber@gmail.com',
+            first_name='Samile',
+            last_name='Seber',
+            user_type=1,
+        )
+    def test_user_created_correctly(self):
+        """Usuário contém informações corretas"""
+        user1 = CustomUser.objects.get(username="sseber")
+        print(CustomUser.get_full_name(user1))
+
+        self.assertEqual(CustomUser.get_full_name(user1), 'Samile Seber')
